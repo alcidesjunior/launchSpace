8c3f85e606a9b7aabcb234c5ee04cb06ac9e1057
Alcides Junior <alcidesjunior@10-45-48-63.0200.ifce.br.iosda.org>
2018-10-17 09:26:24 -0300

    Initial Commit

---
+++ Space Launch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Space Launch
+//
+//  Created by Alcides Junior on 17/10/18.
+//  Copyright © 2018 Alcides Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
