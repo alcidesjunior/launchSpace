06141d07190b954e2db8b8b0512c0d91212039d8
Alcides Junior <alcidesjunior@10-45-48-63.0200.ifce.br.iosda.org>
2018-10-18 12:11:53 -0300

    fluxo de 2 telas pronto

--- Space Launch/MissionDetailsViewController.swift
+++ Space Launch/MissionDetailsViewController.swift
@@ -16,16 +16,55 @@ class MissionDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = self.delegateMission?.getMission().missionName
-        // Do any additional setup after loading the view.
+        self.missionImage.downloadedFrom(link: self.delegateMission?.getMission().links?.missionPatch)
+        missionDetailsTableView.delegate = self
+        missionDetailsTableView.dataSource = self
+        missionDetailsTableView.rowHeight = 70
     }
 
 }
+//detailsMissionCellID
 extension MissionDetailsViewController: UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = missionDetailsTableView.dequeueReusableCell(withIdentifier: "detailsMissionCellID") as! MissionDetailsTableViewCell
+        switch indexPath.row {
+        case 0:
+            let date = Date(timeIntervalSince1970: Double((self.delegateMission?.getMission().launchDateUnix)!))
+            let dateFormatter = DateFormatter()
+            dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+            dateFormatter.locale = NSLocale.current
+            dateFormatter.dateFormat = "dd/MM/yyyy"
+            let launchDate = dateFormatter.string(from: date)
+            
+            cell.labelTitle.text = "Launch date"
+            cell.labelSubTitlte.text = launchDate
+            
+        case 1:
+            cell.labelTitle.text = "Max tentative precision"
+            cell.labelSubTitlte.text = self.delegateMission?.getMission().tentativeMaxPrecision
+        case 2:
+            cell.labelTitle.text = "Launch success"
+            cell.labelSubTitlte.text = (self.delegateMission?.getMission().launchSuccess==true ? "Yes" : "No")
+        case 3:
+            cell.labelTitle.text = "Details"
+            cell.labelSubTitlte.text = self.delegateMission?.getMission().details
+        case 4:
+            cell.labelTitle.text = "About Rocket"
+            cell.labelSubTitlte.text = ""
+            cell.accessoryType = .disclosureIndicator
+        default:
+            0
+        }
+        return cell
     }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.item == 4{
+            print("linha certa", self.delegateMission?.getMission().rocket?.rocketID)
+        }
+    }
+
 }
--- Space Launch/MissionsTableViewCell.swift
+++ Space Launch/MissionsTableViewCell.swift
@@ -46,25 +46,4 @@ class MissionsTableViewCell: UITableViewCell {
 }
 
 
-extension UIImageView {
-    func downloadedFrom(url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
-        contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-                else { return }
-            DispatchQueue.main.async() {
-                self.image = image
-            }
-            }.resume()
-    }
-    func downloadedFrom(link: String?, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
-        if let link = link {
-            guard let url = URL(string: link) else { return }
-            downloadedFrom(url: url, contentMode: mode)
-        }
-    }
-}
+
